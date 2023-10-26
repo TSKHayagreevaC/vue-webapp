7a06b52f44f858acab4594f94454c8076b61aeef
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2023-10-26 12:36:57 +0530

    Added attribute binding.

---
+++ src/components/AttributeBinding.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+const descriptionClass = ref('description')
+</script>
+
+<template>
+  <div>
+    <h1 :class="titleClass">Title</h1> 
+    <p :class="descriptionClass">Description...!</p>
+  </div>
+</template>
+
+<style>
+.title {
+  color: green;
+}
+.description {
+    color: grey;
+}
+</style>
