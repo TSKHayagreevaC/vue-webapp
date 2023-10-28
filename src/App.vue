6fc367d0339edb299d685319eeaf9c4fe813f52c
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2023-10-28 13:24:20 +0530

    Added parent and child components for Emits

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import ParentComponentWithProps from './components/ParentComponentWithProps.vue'
+import ParentComponentWithEmits from './components/ParentComponentWithEmits.vue'
 </script>
 
 <template>
   <h1>HelloWorld</h1>
-  <ParentComponentWithProps />
+  <ParentComponentWithEmits />
 </template>
---
+++ src/components/childComponents/ChildComponentWithEmits.vue
@@ -0,0 +1,8 @@
+<script setup>
+    const emit = defineEmits(['response']);
+    emit('response', 'Hello from Child');
+</script>
+
+<template>
+    <h1>Child Component With Emits</h1>
+</template>
